365d84fc39a9f7d8fce7a87861e60991258f59b6
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-04-12 17:35:35 +0530

    home page added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from './views/home';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
 )
